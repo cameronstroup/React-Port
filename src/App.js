de142f4280657e503d46568b6a9ab570da82a865
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-06-27 14:12:24 -0500

    testing

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Intro from "./components/intro/Intro";
+
+export default function App() {
+  return (
+    <div>
+      <Intro />
+    </div>
+  );
+}
---
+++ src/components/intro/Intro.js
@@ -0,0 +1,12 @@
+import "./intro.css";
+
+const Intro = () => {
+  return (
+    <div className="Intro">
+      <div className="intro-left">Left </div>
+      <div className="intro-right">Right </div>
+    </div>
+  );
+};
+
+export default Intro;
