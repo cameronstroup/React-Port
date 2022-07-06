bbb44a06aae0bb0b59ee06d7a876bd90b3933ee2
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-07-06 11:17:46 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -6,18 +6,23 @@ import ProjectList from "./components/intro/projectsList/projectList";
 import Footer from "./components/footer/Footer";
 import Resume from "./components/resume/Resume";
 import ContactForm from "./components/contactSection/ContactMe";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
       <Navbar />
+
       <Routes>
         <Route path="/aboutMe" element={<Intro />} />
-        <Route path="/React-Port/" element={<Intro />} />
+
         <Route path="/projects" element={<ProjectList />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/contactMe" element={<ContactForm />} />
+        <Route
+          path="/React-Port"
+          element={<Navigate replace to="/aboutMe" />}
+        />
       </Routes>
       <Footer />
     </div>
