4cc43c583ef7b3f135d6d4392cb91f26ea4aa351
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-07-06 09:35:20 -0500

    fixed landing page

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/aboutMe" element={<Intro />} />
-        <Route path="/React-Port" element={<Intro />} />
+        <Route path="/React-Port/" element={<Intro />} />
         <Route path="/projects" element={<ProjectList />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/contactMe" element={<ContactForm />} />
